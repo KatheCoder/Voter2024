79c775a2ce9e7047e868dda1c84947217b0c0904
katlego <katlego@i-t-e.co.za>
2024-05-12 14:33:42 +0200

    import fix

--- app/Imports/RespondentsImport.php
+++ app/Imports/RespondentsImport.php
@@ -3,44 +3,34 @@
 namespace App\Imports;
 
 use App\Models\Respondent;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Validator;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithStartRow;
-use Illuminate\Validation\ValidationException;
-
 
 class RespondentsImport implements ToModel, WithStartRow
 {
+    /**
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     private int $rowCount = 0;
 
-    /**
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function model(array $row): ?Respondent
+    public function model(array $row): Respondent
     {
         $this->rowCount++;
-
-        // Clean data
-        $cleanedRow = array_map(function ($value) {
-            return is_string($value) ? str_replace(',', '', $value) : $value;
-        }, $row);
-
-        try {
-            // Check if the 'sbjNum' field is unique
-            $existingRecord = Respondent::where('sbjNum', $cleanedRow[0])->first();
-            if ($existingRecord) {
-                throw new \Exception("Duplicate entry found for 'sbjNum'.");
-            }
-
-            // Create the record
-            return Respondent::create($cleanedRow);
-        } catch (\Exception $e) {
-            // Log the error
-            Log::error('Error occurred during import: ' . $e->getMessage());
-
-            throw ValidationException::withMessages(['sbjNum' => ["Duplicate entry found for 'sbjNum'."]]);
-        }
+        return new Respondent([
+            'sbjNum' => $this->cleanData($row[0]),
+            'municipality' => $this->cleanData($row[1]),
+            'race' => $this->cleanData($row[2]),
+            'age_groups' => $this->cleanData($row[3]),
+            'gender' => $this->cleanData($row[4]),
+            'likely' => $this->cleanData($row[5]),
+            'decided' => $this->cleanData($row[6]),
+            'voting_status' => $this->cleanData($row[7]),
+            'national' => $this->cleanData($row[8]),
+            'provincial' => $this->cleanData($row[9]),
+            'weight' => $this->cleanData($row[10]),
+        ]);
     }
 
     public function getRowCount(): int
@@ -48,9 +38,12 @@ class RespondentsImport implements ToModel, WithStartRow
         return $this->rowCount;
     }
 
+    private function cleanData($data): array|string|null
+    {
+        return str_replace( ',', '', $data);
+    }
     public function startRow(): int
     {
         return 2;
     }
 }
-
