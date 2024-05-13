6296d8d3846c5d12fb409a6dfbe98eb00fc94e08
katlego <katlego@i-t-e.co.za>
2024-05-13 16:23:58 +0200

    update seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,27 +16,33 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $developer = User::create([
-            'name' => 'Developer',
-            'email' => 'katlego.majatladi@gmail.com',
-                'password' => bcrypt('DeveloperPassword'),
+        $developer1 = User::create([
+            'name' => 'Steven',
+            'email' => 'steven@plus94.co.za',
+                'password' => bcrypt('steven123!@'),
+        ]);
+        $developer2 = User::create([
+            'name' => 'dp',
+            'email' => 'dp@plus94.co.za',
+            'password' => bcrypt('dp123!@'),
         ]);
 
         $adminRole = Role::where('name', 'admin')->first();
 
         if ($adminRole) {
-            $developer->roles()->attach($adminRole);
+            $developer1->roles()->attach($adminRole);
+            $developer2->roles()->attach($adminRole);
         }
-        $user = User::create([
-            'name' => 'Ancgp',
-            'email' => 'Ancgp@anc.co.za',
-            'password' => bcrypt('password'),
-        ]);
-
-        $userRole = Role::where('name', 'user')->first();
-
-        if ($userRole) {
-            $user->roles()->attach($userRole);
-        }
+//        $user = User::create([
+//            'name' => 'Ancgp',
+//            'email' => 'Ancgp@anc.co.za',
+//            'password' => bcrypt('password'),
+//        ]);
+//
+//        $userRole = Role::where('name', 'user')->first();
+//
+//        if ($userRole) {
+//            $user->roles()->attach($userRole);
+//        }
     }
 }
