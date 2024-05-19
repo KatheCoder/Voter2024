a51173bf658382f69685021fc0206dbcd28701de
katlego <katlego@i-t-e.co.za>
2024-05-19 10:24:59 +0200

    refactor and add better logging

--- app/Http/Controllers/RespondentController.php
+++ app/Http/Controllers/RespondentController.php
@@ -1,34 +1,42 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\Imports\RespondentsImport;
 use App\Models\Respondent;
 use Illuminate\Http\Request;
-use \Maatwebsite\Excel\Facades\Excel;
+use Maatwebsite\Excel\Facades\Excel;
 use Throwable;
+use Illuminate\Support\Facades\Log;
 
 class RespondentController extends Controller
 {
     public function import(Request $request, UploadRecordController $uploadRecordController)
     {
         try {
-            if ($request->hasFile('file') && $request->next_update ) {
-                $file = $request->file('file');
-                Respondent::truncate();
+            $request->validate([
+                'file' => 'required|file|mimes:xlsx,csv',
+                'next_update' => 'required|date',
+            ]);
 
-                $import = new RespondentsImport();
-                Excel::import($import, $file);
-                $importedRowsCount = $import->getRowCount();
-                $uploadRecordController->handleUploadRecord(auth()->id(), $importedRowsCount,$request->next_update );
+            $file = $request->file('file');
+            $nextUpdate = $request->input('next_update');
 
-                return redirect()->route('admin')->with('success', $importedRowsCount . ' respondent(s) imported successfully.');
-            } else {
-                return redirect()->route('admin')->with('error', 'No file uploaded.');
-            }
+            Respondent::truncate();
+
+            $import = new RespondentsImport();
+            Excel::import($import, $file);
+            $importedRowsCount = $import->getRowCount();
+
+            $uploadRecordController->handleUploadRecord(auth()->id(), $importedRowsCount, $nextUpdate);
+
+            return redirect()->route('admin')->with('success', $importedRowsCount . ' respondent(s) imported successfully.');
         } catch (Throwable $e) {
-            return  redirect()->route('admin')->with('error', 'An error occurred while importing the file.');
+            Log::error('An error occurred while importing the file: ' . $e->getMessage(), [
+                'exception' => $e
+            ]);
+
+            return redirect()->route('admin')->with('error', 'An error occurred while importing the file. Please check the logs for more details.');
         }
     }
-
 }
+
